eb1482a603a9b3f4b30cc6a96453ea95d9bec4dc
ngtquavn <49022480+ngtquavn@users.noreply.github.com>
2020-06-18 11:27:27 +0700

    update install_telegraf.sh

--- install_telegraf.sh
+++ install_telegraf.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-cat <<EOF | sudo tee /etc/yum.repos.d/influxdb.repo
-[influxdb]
-name = InfluxDB Repository - RHEL \$releasever
-baseurl = https://repos.influxdata.com/rhel/\$releasever/\$basearch/stable
-enabled = 1
-gpgcheck = 1
-gpgkey = https://repos.influxdata.com/influxdb.key
-EOF
 
-sudo yum -y install telegraf
+wget https://dl.influxdata.com/telegraf/releases/telegraf-1.14.4-1.x86_64.rpm
+sudo yum localinstall telegraf-1.14.4-1.x86_64.rpm
 sudo mkdir -p /etc/telegraf
 sudo cp telegraf_centower.conf /etc/telegraf/telegraf.conf
 sudo systemctl start telegraf
