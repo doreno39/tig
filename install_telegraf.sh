88df15eef84e3799e2513276de5559e52d8e2a7d
ngtquavn <49022480+ngtquavn@users.noreply.github.com>
2020-06-18 11:32:08 +0700

    update

--- install_telegraf.sh
+++ install_telegraf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://dl.influxdata.com/telegraf/releases/telegraf-1.14.4-1.x86_64.rpm
-sudo yum localinstall telegraf-1.14.4-1.x86_64.rpm
+sudo yum -y localinstall telegraf-1.14.4-1.x86_64.rpm
 sudo mkdir -p /etc/telegraf
 sudo cp telegraf_centower.conf /etc/telegraf/telegraf.conf
 sudo systemctl start telegraf
