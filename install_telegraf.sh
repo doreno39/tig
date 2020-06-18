c47837f6f20395dacbb1a1825eca52d6d396347d
ngtquavn <49022480+ngtquavn@users.noreply.github.com>
2020-06-18 11:23:49 +0700

    change yum install

--- install_telegraf.sh
+++ install_telegraf.sh
@@ -8,7 +8,7 @@ gpgcheck = 1
 gpgkey = https://repos.influxdata.com/influxdb.key
 EOF
 
-sudo yum install telegraf -y
+sudo yum -y install telegraf
 sudo mkdir -p /etc/telegraf
 sudo cp telegraf_centower.conf /etc/telegraf/telegraf.conf
 sudo systemctl start telegraf
