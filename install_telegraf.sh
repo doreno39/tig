5678e342bd1cc7fb1fbee00063ab55372c699c23
phuongnl15395 <nlphuong1995@gmail.com>
2020-06-24 16:47:04 +0700

    roll back original

--- install_telegraf.sh
+++ install_telegraf.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo systemctl disable exporter_node.service
-sudo systemctl disable exporter_merge.service
-sudo systemctl disable exporter_process.service
-sudo systemctl stop exporter_node.service
-sudo systemctl stop exporter_merge.service
-sudo systemctl stop exporter_process.service
 wget https://dl.influxdata.com/telegraf/releases/telegraf-1.14.4-1.x86_64.rpm
 sudo yum -y localinstall telegraf-1.14.4-1.x86_64.rpm
 sudo mkdir -p /etc/telegraf
